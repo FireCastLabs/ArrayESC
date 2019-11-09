ccc509b34376f6bbbe6731a45d8db6c98f5c6b18
Walt Sorensen <photodude@users.noreply.github.com>
2019-11-08 20:49:30 -0700

    Only set chip frequencies once
    - Set Oscillator Frequency to get a closer to 60 Hz servo signal
    - note where we switch I2C PCA9685 PWM/Servo extender chips in the setup
    - increase final delay before main loop
    - add setting all motors to minimum speed after delay and before the loop we found this was required to make the ESCs work consistently

--- ArrayESC.ino
+++ ArrayESC.ino
@@ -28,7 +28,7 @@ int oESC;  // Variable for the speed sent to the ESC
  * 8 ESC/motors per I2C PWM/Servo extender, 16 signals per extender 2 lines per ESC with 1 for motor and 1 for reverse pin
  * Total 4 I2C PWM/Servo extenders for the 25 motors leaving 13 available signals
  */
-I2C_ESC motorA (0x40, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE);
+I2C_ESC motorA (0x40, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE); //First chip set of 8 motors
 I2C_ESC motorB (0x40, 2, 3, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorC (0x40, 4, 5, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorD (0x40, 6, 7, SPEED_MIN, SPEED_MAX, ARM_VALUE);
@@ -36,7 +36,7 @@ I2C_ESC motorE (0x40, 8, 9, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorF (0x40, 10, 11, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorG (0x40, 12, 13, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorH (0x40, 14, 15, SPEED_MIN, SPEED_MAX, ARM_VALUE);
-I2C_ESC motorI (0x41, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE);
+I2C_ESC motorI (0x41, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE); //Second chip set of 8 motors
 I2C_ESC motorJ (0x41, 2, 3, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorK (0x41, 4, 5, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorL (0x41, 6, 7, SPEED_MIN, SPEED_MAX, ARM_VALUE);
@@ -44,7 +44,7 @@ I2C_ESC motorM (0x41, 8, 9, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorN (0x41, 10, 11, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorO (0x41, 12, 13, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorP (0x41, 14, 15, SPEED_MIN, SPEED_MAX, ARM_VALUE);
-I2C_ESC motorQ (0x42, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE);
+I2C_ESC motorQ (0x42, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE); //Third chip set of 8 motors
 I2C_ESC motorR (0x42, 2, 3, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorS (0x42, 4, 5, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorT (0x42, 6, 7, SPEED_MIN, SPEED_MAX, ARM_VALUE);
@@ -52,44 +52,60 @@ I2C_ESC motorU (0x42, 8, 9, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorV (0x42, 10, 11, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorW (0x42, 12, 13, SPEED_MIN, SPEED_MAX, ARM_VALUE);
 I2C_ESC motorX (0x42, 14, 15, SPEED_MIN, SPEED_MAX, ARM_VALUE);
-I2C_ESC motorY (0x43, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE);
+I2C_ESC motorY (0x43, 0, 1, SPEED_MIN, SPEED_MAX, ARM_VALUE); //Forth chip set of 8 motors
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("ESC I2C control!");
 
-  // Analog servos run at ~60 Hz updates, begin prescale at 105 to get approximatly 60Hz
-  motorA.begin(105);
-  motorB.begin(105);
-  motorC.begin(105);
-  motorD.begin(105);
-  motorE.begin(105);
-  motorF.begin(105);
-  motorG.begin(105);
-  motorH.begin(105);
-  motorI.begin(105);
-  motorJ.begin(105);
-  motorK.begin(105);
-  motorL.begin(105);
-  motorM.begin(105);
-  motorN.begin(105);
-  motorO.begin(105);
-  motorP.begin(105);
-  motorQ.begin(105);
-  motorR.begin(105);
-  motorS.begin(105);
-  motorT.begin(105);
-  motorU.begin(105);
-  motorV.begin(105);
-  motorW.begin(105);
-  motorX.begin(105);
-  motorY.begin(105);
+  /*
+   * Analog servos run at ~60 Hz updates, begin prescale at 105 to get approximatly 60Hz
+   * Very importaint to only enter prescale for the PWM frequency with one motor on the chip.
+   * Multiple setting of the frequency on the I2C PCA9685 PWM/Servo extender chip is likely to cause
+   * unusual behavior
+   */
+  motorA.begin(105); // First Chip set
+  motorB.begin();
+  motorC.begin();
+  motorD.begin();
+  motorE.begin();
+  motorF.begin();
+  motorG.begin();
+  motorH.begin();
+  motorI.begin(105); // Second Chip set
+  motorJ.begin();
+  motorK.begin();
+  motorL.begin();
+  motorM.begin();
+  motorN.begin();
+  motorO.begin();
+  motorP.begin();
+  motorQ.begin(105); // Third Chip set
+  motorR.begin();
+  motorS.begin();
+  motorT.begin();
+  motorU.begin();
+  motorV.begin();
+  motorW.begin();
+  motorX.begin();
+  motorY.begin(105); // Fourth chip set
 
-  delay(10);
-  pinMode(13, OUTPUT);  // LED Visual Output
+  /*
+   * In theory the internal oscillator (clock) is 25MHz but it really isn't that precise. 
+   * You can 'calibrate' by tweaking this number till you get the frequency you're expecting!
+   * The int.osc. is closer to 27MHz and is used for calculating things like writeMicroseconds
+   * We only need to do this once per chipset
+   */
+  motorA.setOscillatorFrequency(26075000); // First Chip set
+  motorI.setOscillatorFrequency(26075000); // Second Chip set
+  motorQ.setOscillatorFrequency(26075000); // Third Chip set
+  motorY.setOscillatorFrequency(26075000); // Fourth chip set
 
-  // Arm the motors
+  delay(10); // Set a small delay to allow the PCA9685 chips time to set their frequency
+  pinMode(13, OUTPUT);  // LED Visual Output pin
+
+  // Arm the ESCs for the motors
   motorA.arm();
   motorB.arm();
   motorC.arm();
@@ -116,8 +132,38 @@ void setup()
   motorX.arm();
   motorY.arm();
 
-  digitalWrite(13, HIGH); // LED High Once Armed
-  delay(1000);  // Wait for a while
+  digitalWrite(13, HIGH); // LED High Once ESCs are Armed
+  delay(5000);  // Wait for a while
+
+  /*
+   * Set ESCs to minimum speed now that the ESCs should be Armed
+   * This is necessary or the ESCs will not be able to work
+   */
+  motorA.speed(SPEED_MIN);
+  motorB.speed(SPEED_MIN);
+  motorC.speed(SPEED_MIN);
+  motorD.speed(SPEED_MIN);
+  motorE.speed(SPEED_MIN);
+  motorF.speed(SPEED_MIN);
+  motorG.speed(SPEED_MIN);
+  motorH.speed(SPEED_MIN);
+  motorI.speed(SPEED_MIN);
+  motorJ.speed(SPEED_MIN);
+  motorK.speed(SPEED_MIN);
+  motorL.speed(SPEED_MIN);
+  motorM.speed(SPEED_MIN);
+  motorN.speed(SPEED_MIN);
+  motorO.speed(SPEED_MIN);
+  motorP.speed(SPEED_MIN);
+  motorQ.speed(SPEED_MIN);
+  motorR.speed(SPEED_MIN);
+  motorS.speed(SPEED_MIN);
+  motorT.speed(SPEED_MIN);
+  motorU.speed(SPEED_MIN);
+  motorV.speed(SPEED_MIN);
+  motorW.speed(SPEED_MIN);
+  motorX.speed(SPEED_MIN);
+  motorY.speed(SPEED_MIN);
 }
 
 void loop()
