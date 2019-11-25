2787c67cfaed1ba4d92c67235c34ab3a3b7b1e8f
photodude <walt@waltsorensen.com>
2019-11-25 09:19:47 -0700

    add example serial parsing
    added for future control development
    Serial parsing example from  Example 5 - Receive with start- and end-markers combined with parsing
      http://forum.arduino.cc/index.php?topic=396450

--- ArrayESC.ino
+++ ArrayESC.ino
@@ -10,8 +10,10 @@
   interface, and 2 pins for power and ground for a total of 4 pins.
   5 wires are used to chain the PWM extenders together
 
+  Serial parsing example from  Example 5 - Receive with start- and end-markers combined with parsing
+  http://forum.arduino.cc/index.php?topic=396450
 
- ****************************************************/
+*****************************************************/
 #include <I2C_ESC.h>
 
 #define LED_PIN (13)            // Pin for the LED 
@@ -23,6 +25,19 @@
 
 int potVal;                     // Variable to read the value from the analog pin
 int oESC;                       // Variable for the speed sent to the ESC
+
+/**
+ * variables for parsing
+ * 
+**/
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars]; // temporary array for use when parsing
+// variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+float floatFromPC = 0.0;
+boolean newData = false;
 
 /*
  * Instantiate the PWM extenders
@@ -60,6 +75,9 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("ESC I2C control!");
+  Serial.println("This demo expects 3 pieces of data - text, an integer and a floating point value");
+  Serial.println("Enter data in this style <HelloWorld, 12, 24.7>  ");
+  Serial.println()
 
   /*
    * Analog servos run at ~60 Hz updates, begin prescale at 105 to get approximatly 60Hz
@@ -159,6 +177,91 @@ void setup()
 }
 
 void loop()
+{
+    recvWithStartEndMarkers();
+
+    if (newData == true)
+    {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            // because strtok() used in parseData() replaces the commas with \0
+        parseData();
+        showParsedData();
+        newData = false;
+    }
+}
+
+// helper functions
+
+//============
+
+void recvWithStartEndMarkers()
+{
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false)
+    {
+        rc = Serial.read();
+
+        if (recvInProgress == true)
+        {
+            if (rc != endMarker)
+            {
+                receivedChars[ndx] = rc;
+                ndx++;
+
+                if (ndx >= numChars)
+                {
+                    ndx = numChars - 1;
+                }
+            }
+            else
+            {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker)
+        {
+            recvInProgress = true;
+        }
+    }
+}
+
+// split the data into its parts
+void parseData()
+{
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars,","); // get the first part - the string
+    strcpy(messageFromPC, strtokIndx);  // copy it to messageFromPC
+
+    strtokIndx = strtok(NULL, ",");   // this continues where the previous call left off
+    integerFromPC = atoi(strtokIndx); // convert this part to an integer
+
+    strtokIndx = strtok(NULL, ",");
+    floatFromPC = atof(strtokIndx);   // convert this part to a float
+}
+
+void showParsedData()
+{
+    Serial.print("Message ");
+    Serial.println(messageFromPC);
+    Serial.print("Integer ");
+    Serial.println(integerFromPC);
+    Serial.print("Float ");
+    Serial.println(floatFromPC);
+}
+
+
+void oldparsing()
 {
   if (Serial.available() > 0) // read the value from the serial
   {
